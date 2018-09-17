01047828a4badcabc6c4f296492203383c4bbd9b
Dan Esparza <esparza.dan@gmail.com>
2018-09-17 15:15:50 -0400

    Add advanced role unit tests

--- data/role_test.go
+++ data/role_test.go
@@ -132,3 +132,83 @@ func TestRole_GetRole_RoleExists_ReturnsRole(t *testing.T) {
 	}
 
 }
+
+func TestRole_AddPoliciesToRole_PolicyDoesntExist_ReturnsError(t *testing.T) {
+
+	//	Arrange
+	systemdb, tokendb := getTestFiles()
+	db, err := data.NewManager(systemdb, tokendb)
+	if err != nil {
+		t.Errorf("NewManager failed: %s", err)
+	}
+	defer func() {
+		db.Close()
+		os.RemoveAll(systemdb)
+		os.RemoveAll(tokendb)
+	}()
+
+	contextUser := data.User{Name: "System"}
+	adminRoleName := "Administrator role"
+
+	//	Act
+
+	//	Add some roles
+	db.AddRole(contextUser, adminRoleName, "Unit test administrator role")
+	db.AddRole(contextUser, "Some other role 1", "Unit test role 1")
+	db.AddRole(contextUser, "Some other role 2", "Unit test role 2")
+	db.AddRole(contextUser, "Some other role 3", "Unit test role 3")
+
+	//	Attempt to add policies that don't exist yet
+	retrole, err := db.AddPoliciesToRole(contextUser, adminRoleName, "policy 1", "policy 2")
+
+	// Sanity check the error
+	// t.Logf("AddPoliciesToRole error: %s", err)
+
+	if len(retrole.Policies) > 0 {
+		t.Errorf("AddPoliciesToRole - Should not have added policies that don't exist to returned role.  Instead, added %v policies", len(retrole.Policies))
+	}
+
+	//	Assert
+	if err == nil {
+		t.Errorf("AddPoliciesToRole - Should throw error attempting to add policies that don't exist but didn't get an error")
+	}
+
+}
+
+func TestRole_AddPoliciesToRole_RoleDoesntExist_ReturnsError(t *testing.T) {
+
+	//	Arrange
+	systemdb, tokendb := getTestFiles()
+	db, err := data.NewManager(systemdb, tokendb)
+	if err != nil {
+		t.Errorf("NewManager failed: %s", err)
+	}
+	defer func() {
+		db.Close()
+		os.RemoveAll(systemdb)
+		os.RemoveAll(tokendb)
+	}()
+
+	contextUser := data.User{Name: "System"}
+	adminRoleName := "Administrator role"
+
+	//	Act
+
+	//	NO ROLES ADDED!
+
+	//	Attempt to add users to group that don't exist yet
+	retrole, err := db.AddPoliciesToRole(contextUser, adminRoleName, "policy 1", "policy 2")
+
+	// Sanity check the error
+	// t.Logf("AddUsersToGroup error: %s", err)
+
+	if len(retrole.Policies) > 0 {
+		t.Errorf("AddPoliciesToRole - Should not have added policies to role that doesn't exist.  Instead, added %v policies", len(retrole.Policies))
+	}
+
+	//	Assert
+	if err == nil {
+		t.Errorf("AddPoliciesToRole - Should throw error attempting to add policies to role that doesn't exist but didn't get an error")
+	}
+
+}
