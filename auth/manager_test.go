20bb36d34f47f168887ace6efe50f9ade099e27b
Dan Esparza <esparza.dan@gmail.com>
2018-09-26 13:32:20 -0400

    Add a test for explicit deny with a wildcard

--- auth/manager_test.go
+++ auth/manager_test.go
@@ -147,3 +147,93 @@ func TestManager_DoPoliciesAllow_InvalidRequest_ReturnsError(t *testing.T) {
 	}
 
 }
+
+func TestManager_DoPoliciesAllow_ExplicitDeny_ReturnsError(t *testing.T) {
+
+	//	Arrange
+	mgr := &auth.Manager{}
+	pols := []data.Policy{
+		{
+			Name:   "Regular user ship access",
+			Effect: policy.Allow,
+			Resources: []string{
+				"Serenity",
+			},
+			Actions: []string{
+				"Find",
+				"Open",
+				"Embark",
+				"Disembark",
+			},
+		},
+		{
+			Name:   "Captain privledges",
+			Effect: policy.Allow,
+			Resources: []string{
+				"Serenity",
+			},
+			Actions: []string{
+				"Fly",
+				"Navigate",
+				"Curse",
+			},
+		},
+		{
+			Name:   "Secret compartment access",
+			Effect: policy.Allow,
+			Resources: []string{
+				"Serenity",
+			},
+			Actions: []string{
+				"AccessCompartments",
+			},
+		},
+		{
+			Name:   "Deny all ship access",
+			Effect: policy.Deny, // Policy deny
+			Resources: []string{
+				"Serenity",
+			},
+			Actions: []string{
+				"<.*>", // Using a regex wildcard
+			},
+		},
+		{
+			Name:   "Healthcare access",
+			Effect: policy.Allow,
+			Resources: []string{
+				"Healthcare",
+			},
+			Actions: []string{
+				"PresentHMOcard",
+				"WaitToSeeDoc",
+				"GetMedicalAdvice",
+			},
+		},
+	}
+
+	//	Act
+	err1 := mgr.DoPoliciesAllow(
+		&data.Request{
+			Action:   "Open",
+			Resource: "Serenity",
+			User:     "malreynolds",
+		}, pols)
+
+	err2 := mgr.DoPoliciesAllow(
+		&data.Request{
+			Action:   "PresentHMOcard",
+			Resource: "Healthcare",
+			User:     "malreynolds",
+		}, pols)
+
+	//	Assert
+	if err1 == nil {
+		t.Errorf("DoPoliciesAllow - should explicitly deny request, but did not get error")
+	}
+
+	if err2 != nil {
+		t.Errorf("DoPoliciesAllow - should allow request, but got error: %v", err2)
+	}
+
+}
